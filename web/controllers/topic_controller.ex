c283ffc61e1b0ceb2f2a982fe9268be33f8691d9
Matheus Mina <mfbmina@gmail.com>
2019-01-09 08:20:59 +0100

    Add feature to create Topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,7 +1,22 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def new(conn, _params) do
-    render conn, "new.html"
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, topic} ->
+        render conn, "show.html", topic: topic
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,8 @@ defmodule Discuss.Router do
     get "/", PageController, :index
 
     get "/topics/new", TopicController, :new
+    get "/topics/#{id}", TopicController, :show
+    post "/topics", TopicController, :create
   end
 
   # Other scopes may use custom stacks.
