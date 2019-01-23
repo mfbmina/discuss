090df7cf6e6ff0e4159f7ea3ebb7002388341cb5
Matheus Mina <mfbmina@gmail.com>
2019-01-23 07:36:47 +0000

    Add controller to Oauth Users using github

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -3,10 +3,35 @@ defmodule Discuss.AuthController do
 
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.inspect "-------------------------------------------------------------------------------"
-    IO.inspect conn.assigns
-    IO.inspect params
-    IO.inspect "-------------------------------------------------------------------------------"
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = _params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: provider}
+    changeset = User.changeset(%User{}, user_params)
+
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+          |> put_flash(:info, "Hello!")
+          |> put_session(:user_id, user.id)
+          |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+          |> put_flash(:error, "Error signing in!")
+          |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
